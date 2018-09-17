bdae88e4a42169842e2315807cc3f6775fc58826
wcperagine <ertyjunior@gmail.com>
2018-09-17 14:55:07 +0000

    roll = true

--- src/components/main/main-components/DiceComponent.js
+++ src/components/main/main-components/DiceComponent.js
@@ -51,7 +51,7 @@ function DiceComponent(props){
     <div id="wrapper">
       <input className="dice-input" id="roll" name="roll" type="checkbox" onClick={diceStop}/>
       <label className="dice-label" id="roll-label" htmlFor="roll"><span></span></label>
-      { roll === false
+      { roll === true
         ? <span></span>
         : giftee_name !== 'Giftee' ? <SelectedDieComponent giftee_name={giftee_name}/> : <span></span> 
       }
