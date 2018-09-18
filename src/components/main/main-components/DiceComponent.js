67162f49d408db1fc6d8f7f31a7ae89a7c0f0ab0
wcperagine <ertyjunior@gmail.com>
2018-09-18 10:41:36 +0000

    merging test to master

--- src/components/main/main-components/DiceComponent.js
+++ src/components/main/main-components/DiceComponent.js
@@ -40,26 +40,46 @@ function DiceComponent(props){
       if( !btn.checked && roll === true ) {
         die.classList.add("select")
         btnLabel.classList.add("select")
+        die.classList.remove("spin")
         btn.disabled = true;
         onGifteeSelect(user_id, spouse_id, group_id)
+      } else if ( btn.checked && roll === true ){
+        die.classList.add("spin")
+      } else if ( !die.classList.contains('dance') && roll === false ){
+        die.classList.add("dance");
+      } else if ( roll === false ) {
+          die.classList.add("dance");
+          die.classList.toggle("pause")
         }
       }
 
+      const returnDice = () => {
+          if (roll === true){
+            return <DieComponent group_id={group_id}/>
+          }
+      }
 
+      const returnSelectedDice = (giftee_name) => {
+        if (roll === true && giftee_name !== 'Giftee'){
+            return <SelectedDieComponent giftee_name={giftee_name}/>
+          }
+      }
+
+      const returnGifteeDice = (roll, giftee_name) => {
+        if (roll === false){
+          return <DieComponent giftee_name={giftee_name}/>
+        }
+      }
 
   return (
     <div id="wrapper">
       <input className="dice-input" id="roll" name="roll" type="checkbox" onClick={diceStop}/>
       <label className="dice-label" id="roll-label" htmlFor="roll"><span></span></label>
-      { roll === false
-        ? <span></span>
-        : giftee_name !== 'Giftee' ? <SelectedDieComponent giftee_name={giftee_name}/> : <span></span> 
-      }
-      { roll === true ? <DieComponent group_id={group_id}/> : <DieComponent giftee_name={giftee_name}/>}
-        
+      { returnDice(roll, giftee_name) }
+      { returnSelectedDice(giftee_name) }
+      { returnGifteeDice(roll, giftee_name) }
     </div>
   );
 }
 
 export default DiceComponent;
-
