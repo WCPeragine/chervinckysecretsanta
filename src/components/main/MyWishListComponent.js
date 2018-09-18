4ddb79bbc0f8c311696e70c1e256e3e3d2a788fb
wcperagine <ertyjunior@gmail.com>
2018-09-18 15:38:21 +0000

    wishlist changes

--- src/components/main/MyWishListComponent.js
+++ src/components/main/MyWishListComponent.js
@@ -8,16 +8,26 @@ function MyWishListComponent (){
  		<WishListBackground/>
     <div id="parchment">
      <ul id="wishlist-ul">
-       <li className="wishlist-li">Sample 1</li>
-       <li className="wishlist-li">Sample 2</li>
-       <li className="wishlist-li">Sample 3</li>
-       <li className="wishlist-li">Sample 4</li>
-       <li className="wishlist-li">Sample 5</li>
-       <li className="wishlist-li">Sample 6</li>
-       <li className="wishlist-li">Sample 7</li>
-       <li className="wishlist-li">Sample 8</li>
-       <li className="wishlist-li">Sample 9</li>
-       <li className="wishlist-li">Sample 10</li>
+       <li className="wishlist-li">
+         <button type='button' className="editBtn">Edit</button>
+         <a href="" className="giftName">Sample 1</a>
+       </li>
+       <li className="wishlist-li">
+         <button type='button' className="editBtn">Edit</button>
+         <a href="" className="giftName">Sample 2</a>
+       </li>
+       <li className="wishlist-li">
+         <button type='button' className="editBtn">Edit</button>
+         <a href="" className="giftName">Sample 3</a>
+       </li>
+       <li className="wishlist-li">
+         <button type='button' className="editBtn">Edit</button>
+         <a href="" className="giftName">Sample 4</a>
+       </li>
+       <li className="wishlist-li">
+         <button type='button' className="editBtn">Edit</button>
+         <a href="" className="giftName">Sample 5</a>
+       </li>
      </ul>
     </div>
  	</div>
